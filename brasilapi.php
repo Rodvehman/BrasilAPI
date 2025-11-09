ac05fb7587b4993fe6191eb99328eb82c791d160
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-08 21:32:17 -0300

    Update brasilapi.php

--- brasilapi.php
+++ brasilapi.php
@@ -1,6 +1,8 @@
 <?php
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Origin *");
+    header("Access-Control-Allow-Methods: GET");
+    
     $cep = $_GET['cep'] ?? null;
     
 ?>
