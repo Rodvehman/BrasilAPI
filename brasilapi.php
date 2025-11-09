f0d1b50e4c7d98decb0bfb0a1af8cbddefea3ad5
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-09 15:21:32 -0300

    Update brasilapi

--- brasilapi.php
+++ brasilapi.php
@@ -2,7 +2,17 @@
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Origin *");
     header("Access-Control-Allow-Methods: GET");
+
+     $url = "https://brasilapi.com.br/api/cep/v1/".$cep;
+
+    $dados = file_get_contents($url);
     
     $cep = $_GET['cep'] ?? null;
     
+    function consultarCEP($cep){
+        $cepCorrigido = preg_replace('/[^0-9]', '', $cep);
+        if (strlen($cepCorrigido) > 8){
+            echo "CEP informado inválido. Somente poderá possuir 8 dígitos.";
+        }
+    }
 ?>
--- cliente.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $url = "https://brasilapi.com.br/api/cep/v1".$cep;
-
-    
-
-    
